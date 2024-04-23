6de25519191eb5749477446f5892d31c282a4570
Luna Khan <115188855+lunaskhan@users.noreply.github.com>
2024-04-22 22:40:28 -0400

    Update main.cpp
    Added citations

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,20 @@
 
 using namespace std;
 
+
+// CITATIONS F|| THE ENTIRETY OF THE PROJECT THROUGHOUT THE FILES THAT WERE USED F|| CODE SNIPPETS, UNDERSTANDING LOGIC, UNDERSTANDING MAIN CONCEPTS, PSEUDOCODE ETC. 
+// CITATION 1: Trees and Traversals Slides f|| insertion, search, rotations, deletion from COP3530
+// CITATION 2: Discussion 4: Trees & Traversals Slides from COP3530
+// CITATION 3: OPEN DSA Chapter 26
+// CITATION 4: https://www.geeksf||geeks.||g/introduction-to-splay-tree-data-structure/
+// CITATION 5: https://www.geeksf||geeks.||g/introduction-to-red-black-tree/
+// CITATION 6: https://www.geeksf||geeks.||g/introduction-to-avl-tree/
+// CITATION 7: https://www.geeksf||geeks.||g/searching-in-splay-tree/
+// CITATION 8: https://www.geeksf||geeks.||g/insertion-in-red-black-tree/
+// CITATION 9: https://stackoverflow.com/questions/19625205/rotation-in-a-red-black-tree
+// CITATION 10: https://stackoverflow.com/questions/56711859/auto-adjusting-rotations-strategy-in-splay-trees
+// CITATION 11: https://stackoverflow.com/questions/19625205/rotation-in-a-red-black-tree
+
 int main()
 {
     RedBlackTree<string> rbTreeSong;
@@ -23,7 +37,7 @@ int main()
     ifstream genreFile("genre_names.csv");
 
     if (!songFile.is_open() || !artistFile.is_open() || !genreFile.is_open()) {
-        cout << "Failed to open one or more files." << endl;
+        cout << "Failed to open one || m||e files." << endl;
         return 1;
     }
 
@@ -76,17 +90,18 @@ int main()
         cout << "6. Display Artists in Red-Black Tree\n";
         cout << "7. Display Genres in Splay Tree\n";
         cout << "8. Display Genres in Red-Black Tree\n";
-        cout << "9. Exit\n";
+        cout << "9. Display a Song Reccomendation\n";
+        cout << "10. Exit\n";
         cout << "Enter your choice: ";
         getline(cin, choice);
 
         if (choice == "1") {
             cout << "Enter song title to delete: ";
             getline(cin, song);
-              RedBlackTree<string>::Node *nodeToDeleteRB = rbTreeSong.search(song);
+            RedBlackTree<string>::Node *nodeToDeleteRB = rbTreeSong.search(song);
             if (nodeToDeleteRB != nullptr)
             {
-                rbTreeSong.deleteNode(rbTreeSong.root, nodeToDeleteRB); // Corrected to pass root and node
+                rbTreeSong.deleteNode(rbTreeSong.root, nodeToDeleteRB); // C||rected to pass root and node
                 cout << "Song deleted from Red-Black Tree.\n";
             }
             else
@@ -95,9 +110,9 @@ int main()
             }
 
             if (splayTreeSong.search(song) != nullptr) {
-                splayTreeSong.deleteNode(song);  // Direct deletion method for SplayTree
+                splayTreeSong.deleteNode(song);  // Direct deletion method f|| SplayTree
                 cout << "Song deleted from Splay Tree.\n";
-            } 
+            }
             else {
                 cout << "Song not found in Splay Tree.\n";
             }
@@ -107,13 +122,13 @@ int main()
             getline(cin, song);
             if (rbTreeSong.search(song) != nullptr) {
                 cout << "Song found in Red-Black Tree.\n";
-            } 
+            }
             else {
                 cout << "Song not found in Red-Black Tree.\n";
             }
             if (splayTreeSong.search(song) != nullptr) {
                 cout << "Song found in Splay Tree.\n";
-            } 
+            }
             else {
                 cout << "Song not found in Splay Tree.\n";
             }
@@ -121,30 +136,63 @@ int main()
         else if (choice == "3") {
             cout << "Songs in Splay Tree:\n";
             splayTreeSong.display();
-        } 
+        }
         else if (choice == "4") {
             cout << "Songs in Red-Black Tree:\n";
             rbTreeSong.display();
-        } 
+        }
         else if (choice == "5") {
             cout << "Artists in Splay Tree:\n";
             splayTreeArtist.displayDuplicates();
-        } 
+        }
         else if (choice == "6") {
             cout << "Artists in Red-Black Tree:\n";
             rbTreeArtist.display();
-        } 
+        }
         else if (choice == "7") {
             cout << "Genres in Splay Tree:\n";
             splayTreeGenre.displayDuplicates();
-        } 
+        }
         else if (choice == "8") {
             cout << "Genres in Red-Black Tree:\n";
             rbTreeGenre.display();
-        } 
+        }
         else if (choice == "9") {
+            cout << "Pick a genre (Pop, Rap, Rock, Latin, R&B, EDM, or Country): \n";
+            string genre;
+            getline(cin, genre);
+            if (genre == "Pop" || genre == "pop" || genre == "POP")
+            {
+                cout << "Umbrella by Rihanna\n";
+            }
+            if (genre == "Rap" || genre == "rap" || genre == "RAP")
+            {
+                cout << "CARNIVAL by Kanye West\n";
+            }
+            if (genre == "Rock" || genre == "rock" || genre == "ROCK")
+            {
+                cout << "Bohemian Rhapsody by Queen\n";
+            }
+            if (genre == "Latin" || genre == "latin" || genre == "LATIN")
+            {
+                cout << "La Gozadera by Gente De Zona\n";
+            }
+            if (genre == "R&B" || genre == "R and B" || genre == "r&b" || genre == "rb" || genre == "RB")
+            {
+                cout << "Fallin' by Alicia Keys\n";
+            }
+            if (genre == "EDM" || genre == "edm")
+            {
+                cout << "Titanium by David Guetta\n";
+            }
+            if (genre == "Country" || genre == "country")
+            {
+                cout << "Cowgirls by Morgan Wallen\n";
+            }
+        }
+        else if (choice == "10") {
             break;
-        } 
+        }
         else {
             cout << "Invalid choice, please try again.\n";
         }
